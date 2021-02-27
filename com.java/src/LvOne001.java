8bc53774155f28df929b4df02c22063403a08409
dev-swd <tladnjsen@gmail.com>
2021-02-27 23:41:54 +0900

    [2021-02-27:SWD] add LvOne001 LvOne001 - 문자열을 정수로 바꾸기

---
+++ com.java/src/LvOne001.java
@@ -0,0 +1,36 @@
+import java.security.cert.TrustAnchor;
+
+public class LvOne001 {
+
+    public int mySolution(String s){
+        return Integer.parseInt(s);
+    }
+
+    public int otherSolution(String str){
+        boolean plus = true;
+        int value = 0;
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '-') {
+                plus = false;
+
+            } else {
+                int num = ch - 48;
+                value = num + value * 10;
+            }
+
+        }
+
+        return (plus==true)? value : -1*value;
+    }
+
+    public static void main(String[] args) {
+        LvOne001 l = new LvOne001();
+
+        System.out.println(l.mySolution("1234"));
+        System.out.println(l.mySolution("-1234"));
+
+        System.out.println(l.otherSolution("1234"));
+        System.out.println(l.otherSolution("-1234"));
+    }
+}
